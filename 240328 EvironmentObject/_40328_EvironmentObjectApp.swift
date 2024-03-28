6b338b1f4ac4b1ad9da8373c02d708d52ff2b9fd
pepilloelgrillo19 <157523378+pepilloelgrillo19@users.noreply.github.com>
2024-03-28 15:14:05 +0100

    Initial Commit

---
+++ 240328 EvironmentObject/_40328_EvironmentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  _40328_EvironmentObjectApp.swift
+//  240328 EvironmentObject
+//
+//  Created by Luis on 28/3/24.
+//
+
+import SwiftUI
+
+@main
+struct _40328_EvironmentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 240328 EvironmentObjectUITests/_40328_EvironmentObjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _40328_EvironmentObjectUITestsLaunchTests.swift
+//  240328 EvironmentObjectUITests
+//
+//  Created by Luis on 28/3/24.
+//
+
+import XCTest
+
+final class _40328_EvironmentObjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
